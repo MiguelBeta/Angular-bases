3828cd8b6b3400a08f2978e502f64e6f9a04d26b
Miguel <workingbeta01@gmail.com>
2024-01-29 16:00:43 -0500

    Bases de angular, comunicacion entre componentes, flujo de datos, reutilizacion de componentes y modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 // import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { HeroesModule } from './heroes/heroes.module';
     CounterModule,
     HeroesModule,
     AppRoutingModule,
+    DbzModule,
 
   ],
   providers: [
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+// import { EventEmitter } from 'stream';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
+
+  onDeleteCharacter(index: number): void {
+    // TODO: Emitir el ID del personaje
+    this.onDelete.emit( index );
+
+  }
+
+}
